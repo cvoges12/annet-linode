8a796de3a9fc5afebce664b16f10660d40b06a75
Clayton Voges <vogesclayton@gmail.com>
2019-02-27 22:16:00 -0600

    added zsh to default shell

--- users.nix
+++ users.nix
@@ -1,19 +1,22 @@
 { config, lib, pkgs, ... }:
 
 {
-  users.extraUsers.cvoges12 = {
-    isNormalUser = true;
-    uid = 1000;
-    description = "Clayton Voges";
-    createHome= true;
-    home = "/home/cvoges12";
-    extraGroups = [
-      "wheel" "disk" "networkmanager"
-      "systemd-journal" "libvirtd"
-    ];
-    shell = pkgs.zsh;
-    packages = with pkgs; [
-      home-manager
-    ];
+  users = {
+    extraUsers.cvoges12 = {
+      isNormalUser = true;
+      uid = 1000;
+      description = "Clayton Voges";
+      createHome= true;
+      home = "/home/cvoges12";
+      extraGroups = [
+        "wheel" "disk" "networkmanager"
+        "systemd-journal" "libvirtd"
+      ];
+      shell = pkgs.zsh;
+      packages = with pkgs; [
+        home-manager
+      ];
+    };
+    defaultUserShell = "/run/current-system/sw/bin/zsh";
   };
 }
