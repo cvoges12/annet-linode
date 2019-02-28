b31a503f32060e4270e5a24e63e414603aff1909
Clayton Voges <vogesclayton@gmail.com>
2019-02-27 21:49:46 -0600

    added fw but not enabled

--- network.nix
+++ network.nix
@@ -4,14 +4,13 @@
   networking = {
     hostName = "annet-linode";
     usePredictableInterfaceNames = false;
+    firewall = {
+      enable = false;
+      allowedTCPPorts = [ 22 53 67 68 80 443 ];
+      allowedUDPPorts = [ 22 53 67 68 80 443 ];
+    };
   };
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 }
