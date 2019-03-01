e1c298b08d9b7ff1b66b6f260e362919d8535396
Clayton Voges <vogesclayton@gmail.com>
2019-03-01 10:34:43 -0600

    updated ssh

--- services.nix
+++ services.nix
@@ -4,6 +4,14 @@
   services.openssh = {
     enable = true;
     allowSFTP = true;
+    hostKeys = [
+      { path = "/etc/ssh/ssh_host_25519_key";
+        rounds = 100;
+        type = "ed25519";}
+    ];
+    passwordAuthentication = true;
     permitRootLogin = "no";
+    ports = [ 22 ];
+    startWhenNeeded = true;
   };
 }
